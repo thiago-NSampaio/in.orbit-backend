7ba88d8a46a4b5e8b35841b41dcc9f127a9437fa
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-10-25 22:21:12 -0400

    deploy

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,28 +1,39 @@
 import { client, db } from ".";
 import { goalCompletions, goals } from "./schema";
+import { fakerPT_BR as faker } from "@faker-js/faker";
 import dayjs from "dayjs";
 
 async function seed() {
   await db.delete(goalCompletions);
   await db.delete(goals);
 
-  const result = await db
+  const [goal1, goal2] = await db
     .insert(goals)
     .values([
-      { title: "Acordar Cedo", desireWeeklyFrequency: 5 },
-      { title: "Caminhar", desireWeeklyFrequency: 4 },
-      { title: "Meditar", desireWeeklyFrequency: 3 },
+      {
+        title: faker.lorem.words(3),
+        desiredWeeklyFrequency: 1,
+      },
+      {
+        title: faker.lorem.words(3),
+        desiredWeeklyFrequency: 2,
+      },
+      {
+        title: faker.lorem.words(3),
+        desiredWeeklyFrequency: 1,
+      },
     ])
     .returning();
 
   const startOfWeek = dayjs().startOf("week");
 
   await db.insert(goalCompletions).values([
-    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
-    { goalId: result[1].id, createdAt: startOfWeek.add(1, "day").toDate() },
+    { goalId: goal1.id, createdAt: startOfWeek.toDate() },
+    { goalId: goal2.id, createdAt: startOfWeek.add(1, "day").toDate() },
   ]);
 }
 
-seed().finally(() => {
+seed().then(() => {
+  console.log("🌱 Database seeded successfully!");
   client.end();
 });
